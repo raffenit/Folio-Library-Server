ea87414954bbd86a23ecc4a04713e116c0eccb2a
Rachael Affenit Hudson <raffenit@gmail.com>
2026-03-30 05:15:27 +0000

    new icons

--- kavita-reader/metro.config.js
+++ kavita-reader/metro.config.js
@@ -2,6 +2,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Remove deprecated Metro watcher option added by older Expo SDK defaults
+delete config.watcher?.unstable_workerThreads;
+
 // Add support for epub.js and other CJS modules
 config.resolver.assetExts.push('db', 'mp3', 'ttf', 'obj', 'png', 'jpg');
 
